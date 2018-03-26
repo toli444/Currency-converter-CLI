803559caf1e4a44a1bb50b2693fae1e4325a6e28
a.semenyaka <anatoly.semenyaka@kandasoft.com>
2018-03-26 18:01:21 +0300

    Add README.md Add .gitignore Handle some exceptions on client side

---
+++ README.md
@@ -0,0 +1,26 @@
+# Currency converter
+
+Fantastic command-line currency converter
+
+## Overview
+
+The project server is developed using thrift. Interface description can be found in currency-converter.thrift
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine
+
+### Installing
+
+First step:
+
+```
+npm install
+```
+
+### Usage
+
+Basic usage is like
+```
+./currency-converter-client.js 10 rub/byn
+```
--- app/currency-converter-client.js
+++ app/currency-converter-client.js
@@ -12,22 +12,33 @@ const connection = thrift.createConnection('localhost', 9090, {
   protocol,
 });
 
-connection.on('error', (err) => {
-  assert(false, err);
-});
-
 const client = thrift.createClient(CurrencyConverter, connection);
 const matchCurrenciesRegExp = /^([a-zA-Z]{3})\/([a-zA-Z]{3})$/i;
 
 const [value, currencies] = process.argv.slice(2);
 const parcedValue = parseFloat(value);
 
+if (!value) {
+    console.log('You should specify a value which you want to convert (example: ./currency-converter-client.js 10 rub/byn');
+    process.exit();
+}
+
+if (!currencies) {
+    console.log('You should specify converting currencies (example: ./currency-converter-client.js 10 rub/byn');
+    process.exit();
+}
+
 let [, currencyFrom, currencyTo] = (currencies && currencies.match(matchCurrenciesRegExp)) || [];
+
+if (!currencyFrom || !currencyTo) {
+  console.log('Invalid currency format. Try something like: byn\/rub');
+  process.exit();
+}
+
 currencyFrom = currencyFrom.toUpperCase();
 currencyTo = currencyTo.toUpperCase();
 
-client.convert(parcedValue, currencyFrom, currencyTo, (err, response) => {
-  console.log(parcedValue);
-  console.log(`result ${response}`);
+client.convert(parcedValue, currencyFrom, currencyTo, (err, responseValue) => {
+  console.log(`${parcedValue} ${currencyFrom} is ${responseValue} ${currencyTo}`);
   process.exit();
 });
